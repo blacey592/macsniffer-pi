fa84aa435533cfea1e2d2807777b49fe6914700e
blacey592 <coachbrianlacey@gmail.com>
2025-05-08 19:45:59 -0700

    Create usb_event.sh

---
+++ usb_logger/usb_event.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LOG_DIR="/home/pi/usb_logger/logs"
+mkdir -p "$LOG_DIR"
+LOG_FILE="$LOG_DIR/usb_log.csv"
+
+ACTION=$(echo "$ACTION")
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
+    (
+        syspath="${sysdevpath%/dev}"
+        devname=$(udevadm info -q name -p "$syspath")
+        [[ "$devname" == "bus/"* ]] && continue
+
+        eval "$(udevadm info -q property --export -p "$syspath")"
+
+        echo "$TIMESTAMP,$ACTION,VID:$ID_VENDOR_ID,PID:$ID_MODEL_ID,$ID_MODEL,$ID_SERIAL_SHORT" >> "$LOG_FILE"
+    )
+done
