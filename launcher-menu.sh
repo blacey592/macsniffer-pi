bb368a161a4fc51bb8797e193e0ba28c9e214e5b
blacey592 <coachbrianlacey@gmail.com>
2025-05-16 17:17:21 -0700

    Update launcher-menu.sh

--- launcher-menu.sh
+++ launcher-menu.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
-clear
-echo "========= MACSniffer-Pi Launcher ========="
-echo "1. Run MAC Sniffer"
-echo "2. Run Bluetooth Logger"
-echo "3. Run GPS Logger (Coming Soon)"
-echo "4. Exit"
-echo ""
-echo "Auto-starting MAC Sniffer in 15 seconds..."
-echo ""
+# MACSniffer-Pi Launcher Menu
+# Unified field deployment interface
 
-read -t 15 -p "Enter choice [1-4]: " choice
+while true; do
+    clear
+    echo "====== MACSNIFFER-PI TOOL LAUNCHER ======"
+    echo " Select a tool to run:"
+    echo ""
+    echo "  1. Bluetooth Logger"
+    echo "  2. Rogue AP Scan"
+    echo "  3. Rogue AP Watch"
+    echo "  4. USB Event Logger"
+    echo "  5. Shutdown"
+    echo ""
+    echo "  0. Exit"
+    echo "========================================="
+    read -p "Enter choice [0-5]: " choice
 
-case "$choice" in
-    1|"")
-        echo "Launching MAC Sniffer..."
-        bash /home/pi/macsniffer-pi/mac-sniffer/sniff.sh
-        ;;
-    2)
-        echo "Launching Bluetooth Logger..."
-        bash /home/pi/macsniffer-pi/bluetooth-logger/bluetooth-logger.sh
-        ;;
-    3)
-        echo "GPS Logger is under development."
-        ;;
-    4)
-        echo "Exiting..."
-        exit 0
-        ;;
-    *)
-        echo "Invalid input. Defaulting to MAC Sniffer..."
-        bash /home/pi/macsniffer-pi/mac-sniffer/sniff.sh
-        ;;
-esac
+    case $choice in
+        1)
+            bash bluetooth/bluetooth-logger.sh
+            read -p "Press Enter to return to menu..."
+            ;;
+        2)
+            bash rogue_ap/rogue-ap-scan.sh
+            read -p "Press Enter to return to menu..."
+            ;;
+        3)
+            bash rogue_ap/rogue_ap_watch.sh
+            read -p "Press Enter to return to menu..."
+            ;;
+        4)
+            bash usb_logger/usb_event.sh
+            read -p "Press Enter to return to menu..."
+            ;;
+        5)
+            echo "Shutting down..."
+            sudo shutdown now
+            ;;
+        0)
+            echo "Exiting launcher. Stay sharp."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice."
+            sleep 1
+            ;;
+    esac
+done
